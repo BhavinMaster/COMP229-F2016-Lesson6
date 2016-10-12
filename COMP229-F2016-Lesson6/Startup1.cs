63d393ec806ff38d8752a12b5305e5a29cb6c33b
BhavinMaster <bmaster@my.centennialcollege.ca>
2016-10-12 19:37:44 -0400

    added identity/authentication

---
+++ COMP229-F2016-Lesson6/Startup1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+//required for owin startup
+using Microsoft.AspNet.Identity;    //authorisation
+using Microsoft.Owin.Security.Cookies; //use owin
+
+[assembly: OwinStartup(typeof(COMP229_F2016_Lesson6.Startup1))]
+
+namespace COMP229_F2016_Lesson6
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Login.aspx")
+            });
+        }
+    }
+}
