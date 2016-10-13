90e8d8d73edf965353b36f0d280d36351a5c898d
BhavinMaster <bmaster@my.centennialcollege.ca>
2016-10-12 20:29:21 -0400

    added login functionality

--- COMP229-F2016-Lesson6/Login.aspx.cs
+++ COMP229-F2016-Lesson6/Login.aspx.cs
@@ -5,6 +5,11 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+//required for identity and owin security
+
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
 namespace COMP229_F2016_Lesson6
 {
     public partial class Login : System.Web.UI.Page
@@ -12,6 +17,38 @@ namespace COMP229_F2016_Lesson6
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void LoginButton_Click(object sender, EventArgs e)
+        {
+            // create new userStore and userManager objects
+
+            var userStore = new UserStore<IdentityUser>();
+            var userManager = new UserManager<IdentityUser>(userStore);
+
+            // Search for and create a new user object
+
+            var user = userManager.Find(UserNameTextBox.Text, PasswordTextBox.Text);
+
+            // if match is found for the user
+
+            if(user != null)
+            {
+                //authenticate and login the user
+                var autheticationManager = HttpContext.Current.GetOwinContext().Authentication;
+                var userIdentity = userManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+
+                //ign in the user
+                autheticationManager.SignIn(new AuthenticationProperties() { IsPersistent = false },userIdentity);
+
+                Response.Redirect("~/Contoso/MainMenu.aspx");
+
+            }
+            else
+            {
+                StatusLabel.Text = "Invalid uname and password";
+                AlertFlash.Visible = true;
+            }
         }
     }
 }
